b9ee241bd5cae3b9bfa527766891cef112c3028e
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-05-18 18:25:53 +0900

    PROL-27 feat: add RiderSimulatedAction n implement a simple rider state machine

--- ras/simulator/helpers.py
+++ ras/simulator/helpers.py
@@ -1,8 +1,23 @@
 from ras.common.messaging import SNSMessage, publish_message
-from ras.rider.schemas import EventMsgRiderUpdated
+from ras.rider.schemas import EventMsgRiderUpdated, RiderState
 from ras.simulator.consts import RIDER_UPDATED_TOPIC_ARN
+from ras.simulator.schemas import RiderSimulatedAction
 
 
 def publish_rider_updated(event_msg: EventMsgRiderUpdated):
     sns_message = SNSMessage(topic_arn=RIDER_UPDATED_TOPIC_ARN, message=event_msg.json())
     return publish_message(sns_message)
+
+
+rider_state_machine = {
+    RiderSimulatedAction.LOGIN: RiderState.AVAILABLE,
+    RiderSimulatedAction.LOGOUT: RiderState.NOT_WORKING,
+    RiderSimulatedAction.ACCEPT_DELIVERY: RiderState.IN_TRANSIT,
+    RiderSimulatedAction.DECLINE_DELIVERY: RiderState.AVAILABLE,
+    RiderSimulatedAction.COMPLETE_DELIVERY: RiderState.AVAILABLE,
+    RiderSimulatedAction.TAKE_A_BREAK: RiderState.BREAK,
+}
+
+
+def get_state_transition(action: RiderSimulatedAction):
+    return rider_state_machine[action]
--- ras/simulator/schemas.py
+++ ras/simulator/schemas.py
@@ -1,3 +1,6 @@
+from enum import Enum
+from typing import Optional
+
 from ninja.schema import Schema
 
 from ras.rider.schemas import Location
@@ -6,3 +9,18 @@ from ras.rider.schemas import Location
 class LocationTriggerPayload(Schema):
     rider_id: int
     location: Location
+
+
+class RiderSimulatedAction(str, Enum):
+    LOGIN = "login"
+    LOGOUT = "logout"
+    ACCEPT_DELIVERY = "accepted"
+    DECLINE_DELIVERY = "declined"
+    COMPLETE_DELIVERY = "completed"
+    TAKE_A_BREAK = "take-a-break"
+
+
+class RiderStatusTriggerPayload(Schema):
+    rider_id: int
+    action: RiderSimulatedAction
+    location: Optional[Location]
