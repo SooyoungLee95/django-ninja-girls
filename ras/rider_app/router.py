0dbe99e97f390f57a915e8af84089918efcc7f19
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-06-07 18:10:49 +0900

    PROL-85 feat: merge rider availability apis n use a condition inside the view to integrate jungleworks
    - remove unnecessary routers

--- ras/rider_app/router.py
+++ ras/rider_app/router.py
@@ -1,9 +1,6 @@
 from ninja import Router
 
-from .views import jungleworks_rider_router, rider_router
+from .views import rider_router
 
 router = Router()
-jw_router = Router()
-
 router.add_router("rider", rider_router, tags=["RiderApp Rider"])
-jw_router.add_router("rider", jungleworks_rider_router, tags=["RiderApp Rider", "Jungleworks"])
--- ras/rider_app/views.py
+++ ras/rider_app/views.py
@@ -2,12 +2,14 @@ from http import HTTPStatus
 
 from ninja.router import Router
 
-from ras.common.integration.services.jungleworks.handlers import on_off_duty
+from ras.common.integration.services.jungleworks.handlers import (
+    on_off_duty,
+    should_connect_jungleworks,
+)
 
 from .schemas import RiderAvailability
 
 rider_router = Router()
-jungleworks_rider_router = Router()
 
 
 @rider_router.put(
@@ -16,14 +18,8 @@ jungleworks_rider_router = Router()
     summary="업무시작/종료",
 )
 async def update_rider_availability(request, data: RiderAvailability):
+    if should_connect_jungleworks(request):
+        return await on_off_duty(data)
+
     # TODO: update rider state after rider model implementation
     return HTTPStatus.OK, {}
-
-
-@jungleworks_rider_router.put(  # type: ignore[no-redef]
-    "/availability",
-    url_name="rider_app_update_rider_availability",
-    summary="[JW]업무시작/종료",
-)
-async def update_rider_availability(request, data: RiderAvailability):  # noqa:F811
-    return await on_off_duty(data)
