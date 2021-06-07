dad69623404d710c6476710ba3fb70cfc97a2eef
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-06-07 17:21:22 +0900

    PROL-85 feat: add routers for rider-app apis
    - in debug mode, explicitly add a router for jungleworks apis for testing
    - in production, add only one router either jungleworks' or ygy's

--- ras/api.py
+++ ras/api.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from ninja import NinjaAPI
 
+from ras.rider_app.router import jw_router as jw_rider_app_router
+from ras.rider_app.router import router as rider_app_router
 from ras.simulator.router import router as simulator_router
 
 api = NinjaAPI(
@@ -8,4 +11,12 @@ api = NinjaAPI(
     version="0.0.1",
 )
 
+if settings.JUNGLEWORKS_ENABLE:
+    api.add_router("rider-app", jw_rider_app_router)
+else:
+    api.add_router("rider-app", rider_app_router)
+
 api.add_router("simulator", simulator_router)
+
+if settings.DEBUG:
+    api.add_router("jw/rider-app", jw_rider_app_router)
