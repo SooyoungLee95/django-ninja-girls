55b38af5c83cb7783b8c394ac8c6c81bccb737e0
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-06-07 12:24:34 +0900

    PROL-85 feat: init rider_app django app

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "ras.rideryo.apps.RideryoConfig",
     "ras.simulator.apps.SimulatorConfig",
+    "ras.rider_app.apps.RiderAppConfig",
 ]
 
 MIDDLEWARE = [
