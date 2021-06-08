7a3975ad6b4d4e8d06349536eaba49a0d8ba56e6
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-06-08 14:04:55 +0900

    PROL-85 feat: map jungleworks error code to api http code

--- ras/common/integration/services/jungleworks/schemas.py
+++ ras/common/integration/services/jungleworks/schemas.py
@@ -1,11 +1,25 @@
+from http import HTTPStatus
+
 from django.conf import settings
 from ninja import Schema
+
+# https://tookanapi.docs.apiary.io/#introduction/error-codes
+_relevant_status = {
+    101: HTTPStatus.BAD_REQUEST,  # INVALID_KEY
+    100: HTTPStatus.BAD_REQUEST,  # PARAMETER_MISSING
+    200: HTTPStatus.OK,  # ACTION_COMPLETE
+    201: HTTPStatus.BAD_REQUEST,  # SHOW_ERROR_MESSAGE
+    404: HTTPStatus.BAD_REQUEST,  # ERROR_IN_EXECUTION
+}
 
 
 class JungleworksResponseBody(Schema):
     message: str
     status: int
     data: dict
+
+    def relevant_http_status(self) -> HTTPStatus:
+        return _relevant_status[self.status]
 
 
 class JungleworksRequestBody(Schema):
