a07b247c2e3873736fedbdaf69f474e9bce0c6a9
Taehyoung.Kwon <taehyoung.kwon@deliveryhero.co.kr>
2021-06-18 15:50:31 +0900

    PROL-35 fix import statement to * in local.py and production.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,6 +1,6 @@
-from .base import ALLOWED_HOSTS, env
+from .base import *  # noqa: F403
 
 ALLOWED_HOSTS += ["host.docker.internal"]  # noqa: F405
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")  # noqa: F405
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,4 @@
-from .base import env
+from .base import *  # noqa: F403
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")  # noqa: F405
