dae81b3022f45d83e07bd37a6b482e650f4b6efd
Taehyoung.Kwon <taehyoung.kwon@deliveryhero.co.kr>
2021-06-30 07:16:19 +0900

    PROL-81 change Response format on validation_error_handler

--- ras/helpers.py
+++ ras/helpers.py
@@ -5,6 +5,6 @@ from ninja.responses import Response
 
 def validation_error_handler(request, exc):
     return Response(
-        {"errors": {"message": "요청 유효성 에러가 발생 하였습니다."}},
+        {"message": exc.errors[0]["msg"]},
         status=HTTPStatus.UNPROCESSABLE_ENTITY,
     )
