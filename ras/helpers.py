7a144ceba647b92d96b97aa8d0618ca66be3598f
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-07-01 14:56:58 +0900

    PROL-290 chore: update validation error handler to log error and send only message data

--- ras/helpers.py
+++ ras/helpers.py
@@ -1,10 +1,12 @@
+import logging
 from http import HTTPStatus
 
 from ninja.responses import Response
 
+logger = logging.getLogger(__name__)
+
 
 def validation_error_handler(request, exc):
-    return Response(
-        {"errors": [{"name": ".".join(err["loc"]), "message": f"{err['type']}: {err['msg']}"} for err in exc.errors]},
-        status=HTTPStatus.UNPROCESSABLE_ENTITY,
-    )
+    errors = [f"{err['type']}:{err['msg']}" for err in exc.errors]
+    logger.error(f"[ApiValidationError {request.method} {request.path}] {','.join(errors)}")
+    return Response(data={"message": "요청을 처리할 수 없습니다."}, status=HTTPStatus.UNPROCESSABLE_ENTITY)
