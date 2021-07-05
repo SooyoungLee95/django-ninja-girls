e80ef4c0ae5af7782da9aeb024df7899dfaead33
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-07-05 17:12:57 +0900

    PROL-314 feat: add push action enums

--- ras/rider_app/enums.py
+++ ras/rider_app/enums.py
@@ -4,3 +4,8 @@ from enum import Enum
 class WebhookName(str, Enum):
     AUTO_ALLOCATION_SUCCESS = "auto_allocation_success"
     MOCK_AUTO_ALLOCATION_SUCCESS = "task_update"
+
+
+class PushAction(str, Enum):
+    NEAR_PICKUP = "dispatch-request:near-pickup"
+    DISPATCHED = "dispatch-request:dispatched"
