9e6a0260bcb402f21dd03e436a3963b77d6ac165
Taehyoung.Kwon <taehyoung.kwon@deliveryhero.co.kr>
2021-07-08 10:39:50 +0900

    PROL-80 add AUTHYO info in test.py for passing test

--- config/settings/test.py
+++ config/settings/test.py
@@ -3,9 +3,14 @@ from .base import *  # noqa: F401,F403
 SECRET_KEY = "GFog0tYABTssUAuZyvh0os6nCxyfJeVWyXFXv5nrZid3AmbEZpDFxQo5998UWTMm"  # mock SECRET_KEY for passing test
 
 
-AUTHYO_SECRET_KEY = """-----BEGIN PUBLIC KEY-----
+AUTHYO = SimpleNamespace(  # noqa: F405
+    BASE_URL="https://staging-authyo.yogiyo.co.kr",
+    FERNET_CRYPTO_KEY="azFuf3CpHYihwAUs5Cf0-_S3QJVfi-ZbS9rkJtIdkHI=".encode(),
+    SECRET_KEY="""-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCJcExoaKfVlIBH6q2IyFbPOCRS
 5+JfIQWDou3wQ2JadCkglsc83g0rc0Yvk8Z9sbFdsi3wnL3dO+3/yklpNO19qICe
 8ga4bAry70xQNCzxw2GZ8+6jNFB8vhZ7q24rd27GCP+IKX/sSvi6YU6zkv9UJno9
 M4ER4mAIz2cETX7rbQIDAQAB
------END PUBLIC KEY-----"""
+-----END PUBLIC KEY-----""",
+    ALGORITHM="RS256",
+)
