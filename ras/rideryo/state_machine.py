add133ce08d866c4fc84c7772669b9b472483906
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-07-21 16:14:34 +0900

    PROL-339 feat: update rider state machine
    - remove singleton pattern and take rider state model instance as input

--- ras/rideryo/state_machine.py
+++ ras/rideryo/state_machine.py
@@ -1,46 +1,54 @@
 from transitions import Machine
 
+from ras.rideryo.enums import RiderState
+
+INITIAL = RiderState.INITIAL
+APPLYING = RiderState.APPLYING
+AVAILABLE = RiderState.AVAILABLE
+STARTING = RiderState.STARTING
+READY = RiderState.READY
+ENDING = RiderState.ENDING
+BREAK = RiderState.BREAK
+PENDING = RiderState.PENDING
+
 
 class RiderStateMachine(Machine):
-    def __new__(cls):
-        if not hasattr(cls, "_instance"):
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def __init__(self):
-        cls = type(self)
-        if not hasattr(cls, "_init"):
-            cls._init = True
-
-        states = ["applying", "available", "starting", "ready", "ending", "break", "pending"]
-        super().__init__(states=states)
+    def __init__(self, model, *args, **kwargs):
+        states = [APPLYING, AVAILABLE, STARTING, READY, ENDING, BREAK, PENDING]
+        super().__init__(
+            model=model,
+            states=states,
+            send_event=True,
+            *args,
+            **kwargs,
+        )
 
         # 라이더 지원요청
-        self.add_transition("apply", "initial", "applying")
+        self.add_transition("apply", INITIAL, APPLYING)
 
         # 입직승인
-        self.add_transition("approve", "applying", "available")
+        self.add_transition("approve", APPLYING, AVAILABLE)
 
         # 근무시작 (온디멘드)
-        self.add_transition("start_work_ondemand", "available", "starting", after="start_dispatch")
+        self.add_transition("start_work_ondemand", AVAILABLE, READY)
 
         # 근무시작 (스케줄)
-        self.add_transition("start_work_schedule", "available", "starting")
+        self.add_transition("start_work_schedule", AVAILABLE, STARTING)
 
         # 신규배차 대기중
-        self.add_transition("start_dispatch", ["starting", "break"], "ready")
+        self.add_transition("start_dispatch", [STARTING, BREAK], READY)
 
         # 신규배차 중지
-        self.add_transition("end_dispatch", "ready", "break")
+        self.add_transition("end_dispatch", READY, BREAK)
 
         # 근무종료
-        self.add_transition("end_work", ["ready", "break"], "ending")
+        self.add_transition("end_work", [READY, BREAK], ENDING)
 
         # 근무 대기
-        self.add_transition("reset", "ending", "available")
+        self.add_transition("reset", ENDING, AVAILABLE)
 
         # 업무정지
-        self.add_transition("block", ["ready", "break"], "pending")
+        self.add_transition("block", [READY, BREAK], PENDING)
 
         # 업무정지 해제
-        self.add_transition("unblock", "pending", "available")
+        self.add_transition("unblock", PENDING, AVAILABLE)
