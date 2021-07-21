60ddacab2c69e887d7ab05bdf061f022ecd2f1c3
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-07-21 18:10:19 +0900

    PROL-339 refactor: simplify rider state usage

--- ras/rideryo/state_machine.py
+++ ras/rideryo/state_machine.py
@@ -1,20 +1,11 @@
 from transitions import Machine
 
-from ras.rideryo.enums import RiderState
-
-INITIAL = RiderState.INITIAL
-APPLYING = RiderState.APPLYING
-AVAILABLE = RiderState.AVAILABLE
-STARTING = RiderState.STARTING
-READY = RiderState.READY
-ENDING = RiderState.ENDING
-BREAK = RiderState.BREAK
-PENDING = RiderState.PENDING
+from ras.rideryo.enums import RiderState as rs
 
 
 class RiderStateMachine(Machine):
     def __init__(self, model, *args, **kwargs):
-        states = [APPLYING, AVAILABLE, STARTING, READY, ENDING, BREAK, PENDING]
+        states = [rs.APPLYING, rs.AVAILABLE, rs.STARTING, rs.READY, rs.ENDING, rs.BREAK, rs.PENDING]
         super().__init__(
             model=model,
             states=states,
@@ -25,37 +16,37 @@ class RiderStateMachine(Machine):
         )
 
         # 라이더 지원요청
-        self.add_transition("apply", INITIAL, APPLYING)
+        self.add_transition("apply", rs.INITIAL, rs.APPLYING)
 
         # 입직승인
-        self.add_transition("approve", APPLYING, AVAILABLE)
+        self.add_transition("approve", rs.APPLYING, rs.AVAILABLE)
 
         # 근무시작 (온디멘드)
-        self.add_transition("start_work_ondemand", AVAILABLE, READY, prepare=self._ondemand_auto_transit)
+        self.add_transition("start_work_ondemand", rs.AVAILABLE, rs.READY, prepare=self._ondemand_auto_transit)
 
         # 근무시작 (스케줄)
-        self.add_transition("start_work_schedule", AVAILABLE, STARTING)
+        self.add_transition("start_work_schedule", rs.AVAILABLE, rs.STARTING)
 
         # 신규배차 대기중
-        self.add_transition("start_dispatch", [STARTING, BREAK], READY)
+        self.add_transition("start_dispatch", [rs.STARTING, rs.BREAK], rs.READY)
 
         # 신규배차 중지
-        self.add_transition("end_dispatch", READY, BREAK)
+        self.add_transition("end_dispatch", rs.READY, rs.BREAK)
 
         # 근무종료
-        self.add_transition("end_work", [READY, BREAK], ENDING)
+        self.add_transition("end_work", [rs.READY, rs.BREAK], rs.ENDING)
 
         # 근무 대기
-        self.add_transition("reset", ENDING, AVAILABLE)
+        self.add_transition("reset", rs.ENDING, rs.AVAILABLE)
 
         # 업무정지
-        self.add_transition("block", [READY, BREAK], PENDING)
+        self.add_transition("block", [rs.READY, rs.BREAK], rs.PENDING)
 
         # 업무정지 해제
-        self.add_transition("unblock", PENDING, AVAILABLE)
+        self.add_transition("unblock", rs.PENDING, rs.AVAILABLE)
 
     def _ondemand_auto_transit(self, *args, **kwargs):
-        self.model.state = STARTING
+        self.model.state = rs.STARTING
         self._save_model()
 
     def _save_model(self, *args, **kwargs):
