ac6b92c2bc3b03a6ee42fff68ba9fd2313a9f76f
Changhyun An <changhyun.an@deliveryhero.co.kr>
2021-08-03 14:00:11 +0900

    PROL-446 feat: implement api for rider actions; map on, off api

---
+++ tests/rider_app/test_action_views.py
@@ -0,0 +1,93 @@
+from enum import Enum
+from http import HTTPStatus
+from unittest.mock import patch
+
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+from ras.rider_app.constants import MSG_INVALID_VALUE, MSG_STATE_MACHINE_CANNOT_PROCESS
+from ras.rideryo.enums import RiderState as RiderStateEnum
+from ras.rideryo.enums import RiderTransition
+
+
+@pytest.mark.django_db(transaction=True)
+def test_action_error_when_invalid_action_is_given(rider_profile, rider_state, mock_jwt_token):
+    # Given: 상태머신에 올바르지 않은 함수명을 등록해둔 경우
+    # - mocking한 상태전환 Enum 생성
+    dummy_enum = Enum("DummyEnum", list(RiderTransition.__members__), type=str)  # type: ignore[misc]
+    with patch("ras.rideryo.state_machine.rt", dummy_enum):
+
+        # When: 라이더 상태 전환액션 API를 호출 하였을 때
+        client = Client()
+        response = client.put(
+            reverse("ninja:trigger_rider_action", kwargs={"action": RiderTransition.ENABLE_NEW_DISPATCH.value}),
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {mock_jwt_token}",
+        )
+
+    # Then: 400 BAD REQUEST 를 return 해야한다
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    data = response.json()
+    assert data["message"] == MSG_INVALID_VALUE
+
+
+@pytest.mark.django_db(transaction=True)
+def test_action_error_when_action_is_not_processable(rider_profile, rider_state, mock_jwt_token):
+    # Given: 호출할 액션에서 전환 불가한 라이더 상태인 경우
+    rider_state.state = RiderStateEnum.APPLYING
+    rider_state.save()
+
+    # When: 라이더 상태 전환액션 API를 호출 하였을 때
+    client = Client()
+    response = client.put(
+        reverse("ninja:trigger_rider_action", kwargs={"action": RiderTransition.ENABLE_NEW_DISPATCH.value}),
+        content_type="application/json",
+        HTTP_AUTHORIZATION=f"Bearer {mock_jwt_token}",
+    )
+
+    # Then: 409 CONFLICT 를 return 해야한다
+    assert response.status_code == HTTPStatus.CONFLICT
+    data = response.json()
+    assert data["message"] == MSG_STATE_MACHINE_CANNOT_PROCESS
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.parametrize("valid_state", (RiderStateEnum.STARTING, RiderStateEnum.BREAK))
+def test_action_enable_new_dispatch(valid_state, rider_profile, rider_state, mock_jwt_token):
+    # Given: 신규 배차 on 가능한 상태에 있는 경우
+    rider_state.state = valid_state
+    rider_state.save()
+
+    # When: 라이더 상태 전환액션 API를 호출 하였을 때
+    client = Client()
+    response = client.put(
+        reverse("ninja:trigger_rider_action", kwargs={"action": RiderTransition.ENABLE_NEW_DISPATCH.value}),
+        content_type="application/json",
+        HTTP_AUTHORIZATION=f"Bearer {mock_jwt_token}",
+    )
+
+    # Then: 200 OK 를 return 해야한다
+    assert response.status_code == HTTPStatus.OK
+    data = response.json()
+    assert data["success"] is True
+
+
+@pytest.mark.django_db(transaction=True)
+def test_action_disable_new_dispatch(rider_profile, rider_state, mock_jwt_token):
+    # Given: 신규 배차 off 가능한 상태에 있는 경우
+    rider_state.state = RiderStateEnum.READY
+    rider_state.save()
+
+    # When: 라이더 상태 전환액션 API를 호출 하였을 때
+    client = Client()
+    response = client.put(
+        reverse("ninja:trigger_rider_action", kwargs={"action": RiderTransition.DISABLE_NEW_DISPATCH.value}),
+        content_type="application/json",
+        HTTP_AUTHORIZATION=f"Bearer {mock_jwt_token}",
+    )
+
+    # Then: 200 OK 를 return 해야한다
+    assert response.status_code == HTTPStatus.OK
+    data = response.json()
+    assert data["success"] is True
